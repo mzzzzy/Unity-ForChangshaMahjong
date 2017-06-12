13fc2ac4f969ef1a0aa1a6f3af262ba7f885f43a
mzzy1029zy <328860643@qq.com>
2017-06-12 17:41:17 +0800

    Three to GitHub
    第三次提交 游戏开始界面的视图已经完成 接下来就是做按钮逻辑等了

---
+++ Assets/Scripts/GameBeginView-Scene/TimeAndElectricity/TimeCurrent.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems ;
+using System ;
+using UnityEngine.UI ;
+
+public class TimeCurrent : MonoBehaviour {
+
+	// 获取实时时间 
+
+	public Text text ;
+
+	public char[] ch =  new char[1] ;
+
+
+
+	void Awake()
+	{
+		ch[0] = ' ' ;
+
+	}
+
+	// Use this for initialization
+	void Start () {
+	   
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+
+
+		string timeCurrent = DateTime.Now.ToString ();
+		string[] arr = timeCurrent.Split (ch);
+
+		text.text = arr[1] ;
+		Debug.Log (arr[1]);
+	}
+}
